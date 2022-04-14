9bf2c36e854c2605997d486cf2b7b452a332fd3a
Jason Francis <97536492+BL-MSCH-C220-S22@users.noreply.github.com>
2022-04-14 10:40:59 -0400

    A few small fixes
    I was able to get your sound playing (check out line 18 in jewel.gd to see what I did).
    
    I also fixed the score signal (also in jewel.gd) so the score label is updating.
    
    I made a few adjustments to capture and release the mouse as well as clearing out the score when restarting.
    
    Hopefully that helps. This looks really great so far. Let me know if I can assist with anything else.

--- Maze/Maze.gd
+++ Maze/Maze.gd
@@ -39,6 +39,7 @@ var height = 12  						# height of map (in tiles)
 
 func _ready():
 	randomize()
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	make_maze()
 	
 func check_neighbors(cell, unvisited):
--- UI/Main Menu.gd
+++ UI/Main Menu.gd
@@ -6,9 +6,9 @@ func _ready():
 
 
 func _on_Play_pressed():
-	get_tree().change_scene("res://Maze/Maze.tscn")
+	var _scene = get_tree().change_scene("res://Maze/Maze.tscn")
 
 
 func _on_Quit_pressed():
 	queue_free()
-	get_tree().change_scene("res://UI/Game Over.tscn")
+	var _scene = get_tree().change_scene("res://UI/Game Over.tscn")
